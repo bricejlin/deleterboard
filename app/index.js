fc6ffdf7fa40b30942c2d7de5bc281e1103eed60
Brice Lin <bricejlin@gmail.com>
2015-04-28 20:51:46 -0400

    use firebase for storage

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
+require('cute-stack')();
+
 var Firebase = require('firebase');
 
 var server = require('server');
 var github = require('github');
 
-// var db = new Firebase('https://deleterboard.firebaseio.com/test');
-// var dbRef = db.child('commits');
+var db = new Firebase('https://deleterboard.firebaseio.com/test');
+var dbRef = db.child('commits');
 
 var INTERVAL_SECS = 30;
 
@@ -14,13 +16,13 @@ server.listen(3000, function () {
   console.log('Listening on port: 3000...');
 
   // setInterval(function () {
-  // console.log('fetching data from api...');
-  // github.getStats()
-  //   .then(function (users) {
-  //     dbRef.set({
-  //       users: users
-  //     });
-  //     console.log('updated db!');
-  //   });
+  console.log('fetching data from api...');
+  github.getStats()
+    .then(function (users) {
+      dbRef.set({
+        users: users
+      });
+      console.log('updated db!');
+    });
   // }, INTERVAL_SECS * 1000);
 });
